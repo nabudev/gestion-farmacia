8f82abfd2db5e2295e45e55e1252c017efdc2e93
nahueldev <nabudev01@gmail.com>
2024-05-27 11:25:10 -0300

    instalacion de jazzmin para customizar el panel de administrador

--- farmacia/settings.py
+++ farmacia/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -122,3 +123,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+JAZZMIN_SETTINGS= {
+    'site_header': "Farmacia",
+    'site_brand': "STOCK Y VENTAS",
+    'copyright': "Desarrollado por Nahuel Flores",
+}
--- myapp/models.py
+++ myapp/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class ObraSocial(models.Model):
+class ObraSociale(models.Model):
     nombre= models.CharField(max_length=45)
     descuento= models.IntegerField
     
@@ -14,9 +14,9 @@ class Cliente (models.Model):
     nombre= models.CharField(max_length=45)
     domicilio= models.CharField(max_length=30)
     telefono= models.IntegerField
-    obra_social= models.ForeignKey(ObraSocial, on_delete=models.CASCADE)
+    obra_social= models.ForeignKey(ObraSociale, on_delete=models.CASCADE)
     
-class CuentaParticular(models.Model):
+class CuentaParticulare(models.Model):
     cliente= models.ForeignKey(Cliente, on_delete=models.CASCADE)
     saldo= models.CharField(max_length=15)
     
@@ -26,31 +26,31 @@ class Laboratorio(models.Model):
 class CompuestoGenerico(models.Model):
     nombre= models.CharField(max_length=45)
     
-class MarcaComercial(models.Model):
+class MarcaComerciale(models.Model):
     nombre= models.CharField(max_length=45)
     
 class PresentacionMedicamento(models.Model):
     tipo= models.CharField(max_length=25)
 
 class Medicamento(models.Model):
-    marca_comercial= models.ForeignKey(MarcaComercial, on_delete=models.CASCADE)
+    marca_comercial= models.ForeignKey(MarcaComerciale, on_delete=models.CASCADE)
     compuesto_generico= models.ForeignKey(CompuestoGenerico, on_delete=models.CASCADE)
     presentacion_medicamento= models.ForeignKey(PresentacionMedicamento, on_delete=models.CASCADE)
     dosificacion= models.CharField(max_length=25)
     cantidad= models.IntegerField
     laboratorio= models.ForeignKey(Laboratorio, on_delete=models.CASCADE)
     
-class Proveedor(models.Model):
+class Proveedore(models.Model):
     apellido= models.CharField(max_length=45)
     nombre= models.CharField(max_length=45)
     telefono= models.IntegerField
     direccion= models.CharField(max_length=30)
     
-class StockProveedor(models.Model):
-    proveedor= models.ForeignKey(Proveedor, on_delete=models.CASCADE)
+class StockProveedore(models.Model):
+    proveedor= models.ForeignKey(Proveedore, on_delete=models.CASCADE)
     medicamento= models.ForeignKey(Medicamento, on_delete=models.CASCADE)
     
-class Ventas(models.Model):
+class Venta(models.Model):
     cliente= models.ForeignKey(Cliente, on_delete=models.CASCADE)
     metodo_pago= models.ForeignKey(MetodoPago, on_delete=models.CASCADE)
     medicamento= models.ForeignKey(Medicamento, on_delete=models.CASCADE)
